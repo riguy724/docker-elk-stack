6ade8a4756729340bba19455a566896e219d0e04
Christopher Riley <riguy724@gmail.com>
2015-11-14 11:51:57 -0500

    elasticsearch - persist logs and data, use copied config

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-mkdir -p /etc/elasticsearch
-mkdir -p /var/lib/elasticsearch
-mkdir -p /var/log/elasticsearch
-
-cp -r /data/etc/* /etc/elasticsearch
-cp -r /data/lib/* /var/lib/elasticsearch
-cp -r /data/log/* /var/log/elasticsearch
-
 supervisord -c /etc/supervisord.conf
